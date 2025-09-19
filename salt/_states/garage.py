ed7df7179d0c9313222e3be4a5060724d1e1b02f
Marcus Rückert <darix@nordisch.org>
2025-09-19 03:13:02 +0200

    remove the exec code as there is an openapi json api we can use instead

--- salt/_states/garage.py
+++ salt/_states/garage.py
@@ -7,22 +7,6 @@ garage_config_path = "/etc/garage/garage.toml"
 
 log = logging.getLogger(__name__)
 
-def _run_subcommand(sub_command_list):
-    cmd = ["/usr/bin/garage", f"--config={garage_config_path}"]
-    cmd.extend(sub_command_list)
-
-    log.error(f"cmd: {cmd}")
-    try:
-        env = os.environ.copy()
-        proc = Popen(cmd, stdout=PIPE, stderr=PIPE, env=env, encoding="utf-8")
-        cmd_output_data, cmd_error_code = proc.communicate()
-        pass_returncode = proc.returncode
-    except (OSError, UnicodeDecodeError) as e:
-        cmd_output_data, cmd_error_code = "", str(e)
-        pass_returncode = 1
-
-    # The version of pass used during development sent output to
-    # stdout instead of stderr even though its returncode was non zero.
-    if pass_returncode or not cmd_output_data:
-        log.error(f"something failed while running garage: c:{pass_returncode} d:{cmd_output_data}")
-    return cmd_output_data.rstrip("\r\n")
+def layout_assignment(zone, datacenter):
+  ret = {'name': name, 'result': None, 'changes': {}, 'comment': ""}
+  return ret
